4d10349204d8d7f65fae15ab1840a8ad11cbb9b6
PauloAlves8039 <pj38alves@gmail.com>
2020-06-29 18:46:04 -0300

    Adicionando projeto no repositório

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,44 @@
+/**
+ * @file: course-list.component.ts
+ * @author: Paulo Alves
+ * @description: compoenente responsável pela listagem dos cursos.
+ * @version 1.0.1 (29/06/2020)
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html'
+})
+
+export class CourseListComponent implements OnInit{
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+          id: 1,
+          name: 'Angular: Forms',
+          imageUrl: '',
+          price: 99.99,
+          code: 'XPS-8796',
+          duration: 120,
+          rating: 4.5,
+          releaseDate: 'March, 2, 2020'
+      },
+      {
+          id: 2,
+          name: 'Angular: HTTP',
+          imageUrl: '',
+          price: 45.99,
+          code: 'LKL-1094',
+          duration: 80,
+          rating: 4,
+          releaseDate: 'April, 4, 2020'
+      }
+    ];
+  }
+
+}
---
+++ src/app/courses/course.ts
@@ -0,0 +1,17 @@
+/**
+ * @file: course.ts
+ * @author: Paulo Alves
+ * @description: responsável por representar os atributos da entidade curso.
+ * @version 1.0.1 (29/06/2020)
+ */
+
+export class Course {
+  id: number;
+  name: string;
+  imageUrl: string;
+  price: number;
+  code: string;
+  duration: number;
+  rating: number;
+  releaseDate: string;
+}
